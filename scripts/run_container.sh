171d8dcb6e171e7b317a74e86f472d1de9227db0
JerryZ <60121916+mingguangZhou@users.noreply.github.com>
2025-04-09 16:00:38 +0200

    Update run_container.sh to create same image name

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -6,8 +6,15 @@ mkdir -p $HOME/f1tenth_ws
 # give docker permission to use X
 sudo xhost +si:localuser:root
 
-# run container with privilege mode, host network, display, and mount workspace on host
+# Stop and remove any container with the same name
+sudo docker rm -f f1tenth_onboard
+
+sudo ln -sf /usr/lib/aarch64-linux-gnu/libffi.so.8.1.0 /usr/lib/aarch64-linux-gnu/libffi.so
+sudo ln -sf /usr/lib/aarch64-linux-gnu/libffi.so.8.1.0 /usr/lib/aarch64-linux-gnu/libffi.so.8
+
+# Start the container with a permanent name and make necessary fixes
 sudo docker run --runtime nvidia -it --privileged --network host -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev \
     --mount type=volume,dst=/f1tenth_ws,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$HOME/f1tenth_ws \
+    --name f1tenth_onboard \
     f1tenth/focal-l4t-foxy:f1tenth-stack
