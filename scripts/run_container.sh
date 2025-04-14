a674723d72ed9acaba6f15e1038ef0b9082f2c28
JerryZ <60121916+mingguangZhou@users.noreply.github.com>
2025-04-14 23:38:30 +0200

    Add ros domain id for broadcasting

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -14,6 +14,7 @@ sudo ln -sf /usr/lib/aarch64-linux-gnu/libffi.so.8.1.0 /usr/lib/aarch64-linux-gn
 
 # Start the container with a permanent name and make necessary fixes
 sudo docker run --runtime nvidia -it --privileged --network host -e DISPLAY=$DISPLAY \
+    -e ROS_DOMAIN_ID=0 \
     -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev \
     --mount type=volume,dst=/f1tenth_ws,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$HOME/f1tenth_ws \
     --name f1tenth_onboard \
