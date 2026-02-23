77b24f878420ce3f4a52586e5c2dd5ce6ee650ac
mingguangZhou <mingguang_zhou@berkeley.edu>
2026-02-23 03:57:13 +0100

    Modified to be same as current working container f1tenth_system on Oudtra00. To be tested.

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -18,4 +18,3 @@ sudo docker run --runtime nvidia -it --privileged --network host -e DISPLAY=1 \
     --mount type=volume,dst=/f1tenth_ws,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$HOME/f1tenth_ws \
     --name f1tenth_onboard \
     f1tenth/focal-l4t-foxy:f1tenth-stack
-
