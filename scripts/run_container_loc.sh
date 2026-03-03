05ff3db53ced8301a1cd3b782329dcc767a238af
mingguangZhou <mingguang_zhou@berkeley.edu>
2026-03-03 09:59:51 +0100

    Add odom_to_baselink tf switch parameter, to be tested for PF

---
+++ scripts/run_container_loc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# create workspace on host, in home directory
+mkdir -p $HOME/f1tenth_ws
+
+# give docker permission to use X
+sudo xhost +si:localuser:root
+
+# Stop and remove any container with the same name
+sudo docker rm -f f1tenth_onboard
+
+sudo ln -sf /usr/lib/aarch64-linux-gnu/libffi.so.8.1.0 /usr/lib/aarch64-linux-gnu/libffi.so
+sudo ln -sf /usr/lib/aarch64-linux-gnu/libffi.so.8.1.0 /usr/lib/aarch64-linux-gnu/libffi.so.8
+
+# Start the container with a permanent name and make necessary fixes
+sudo docker run --runtime nvidia -it --privileged --network host -e DISPLAY=1 \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev \
+    --mount type=volume,dst=/f1tenth_ws,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$HOME/f1tenth_ws \
+    --name f1tenth_onboard \
+    f1tenth_onboard_localization_env:latest
+
