5fed5dffc69c46a56f389035caa1a937cb4c622f
mingguangZhou <mingguang_zhou@berkeley.edu>
2026-04-22 20:00:11 +0200

    change start script X access to run simpler start procedure with VNC

--- scripts/run_container_loc.sh
+++ scripts/run_container_loc.sh
@@ -4,7 +4,7 @@
 mkdir -p $HOME/f1tenth_ws
 
 # give docker permission to use X
-sudo xhost +si:localuser:root
+# sudo xhost +si:localuser:root
 
 # Stop and remove any container with the same name
 sudo docker rm -f f1tenth_onboard
@@ -12,8 +12,12 @@ sudo docker rm -f f1tenth_onboard
 sudo ln -sf /usr/lib/aarch64-linux-gnu/libffi.so.8.1.0 /usr/lib/aarch64-linux-gnu/libffi.so
 sudo ln -sf /usr/lib/aarch64-linux-gnu/libffi.so.8.1.0 /usr/lib/aarch64-linux-gnu/libffi.so.8
 
+# use dummy X display on host
+export DISPLAY=:1
+echo "Using DISPLAY=$DISPLAY"
+
 # Start the container with a permanent name and make necessary fixes
-sudo docker run --runtime nvidia -it --privileged --network host -e DISPLAY=1 \
+sudo docker run --runtime nvidia -it --privileged --network host -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev \
     --mount type=volume,dst=/f1tenth_ws,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$HOME/f1tenth_ws \
     --volume /usr/local/cuda:/usr/local/cuda \
